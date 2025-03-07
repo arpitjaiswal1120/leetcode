2bc919242de630d68499a4ae72bd0498c7b51a29
Arpit Jaiswal <arpit.jaiswal.1120@gmail.com>
2025-03-07 18:10:53 +0530

    Time: 9 ms (6.18%) | Memory: 12.5 MB (11.20%) - LeetSync

---
+++ 1009-pancake-sorting/pancake-sorting.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def pancakeSort(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: List[int]
+        """
+        out = []
+        n = len(arr)
+        for i in range(n-1, -1, -1):
+            max_pos = self.get_max_pos(arr[:i+1])
+            if(max_pos == i):
+                continue
+            out.append(max_pos+1)
+            out.append(i+1)
+            arr = arr[:max_pos+1][::-1] + arr[max_pos+1:]
+            arr = arr[:i+1][::-1] + arr[i+1:]
+        return out
+    
+    def get_max_pos(self, arr):
+        m = 0
+        for i in range(1, len(arr)):
+            if(arr[m] < arr[i]):
+                m = i
+        return m
+    
+        
